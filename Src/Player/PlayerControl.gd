f6e94972de1adc5f0e4af15679d193d9e3aa2dcd
pjimenez1502 <pjimenez1502@gmail.com>
2024-06-18 18:34:12 +0200

    Player movement easing

--- Src/Player/PlayerControl.gd
+++ Src/Player/PlayerControl.gd
@@ -9,9 +9,12 @@ var direction : Vector2
 func _input(_event: InputEvent) -> void:
 	var vertical : float = Input.get_axis("DOWN", "UP")
 	var horizontal : float = Input.get_axis("LEFT", "RIGHT")
-	direction = Vector2(horizontal, vertical)
+	#direction = Vector2(horizontal, vertical)
+	direction = Vector2(easeInSine(horizontal), easeInSine(vertical))
 	
 	if _event.is_action_pressed("SHOOT"):
 		_player_ship.weapon.shoot()
 
 
+func easeInSine(x: float) -> float:
+	return (1 - cos((x * PI) / 2)) * sign(x)
