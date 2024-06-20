02a539814aa04de23d8a84b508e5e4d9cb008f04
pjimenez1502 <pjimenez1502@gmail.com>
2024-06-21 00:18:05 +0200

    Ship Stats

--- Src/Ship/Ship_Stats.gd
+++ Src/Ship/Ship_Stats.gd
@@ -1,5 +1,26 @@
 extends Node
+class_name ship_stats
 
-var hull : float
-var shield : float
+@onready var player_ship: player_ship = $".."
 
+@export var hull : float = 60
+@export var shield : float = 20
+
+func damage(damage : float) -> void:
+	player_ship.animation_player.play("Damage")
+	
+	shield -= damage
+	if shield < 0:
+		hull += shield
+		shield = 0
+	print("hull: ", hull, " shield: ", shield)
+	if hull <= 0:
+		death()
+
+func death() -> void:
+	print("player dead")
+
+var terrain_damage : float = 10
+func _on_hit_area_body_entered(body: Node3D) -> void:
+	damage(terrain_damage)
+	pass # Replace with function body.
