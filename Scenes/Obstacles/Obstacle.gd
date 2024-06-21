ab6ccb8a0381c81e6c9e052ec79906e27347e722
pjimenez1502 <pjimenez1502@gmail.com>
2024-06-21 19:49:13 +0200

    Updated Asteroid

--- Scenes/Obstacles/Obstacle.gd
+++ Scenes/Obstacles/Obstacle.gd
@@ -2,16 +2,22 @@ extends StaticBody3D
 class_name obstacle
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var asteroid: Node3D = $Asteroid
 
-@export var speed : float = 4
+
+@export var BASE_SPEED : float = 4
+var speed : float
 @export var hp : float = 50
 @export var lifetime : float = 30
 @export var value : int = 10
 
+var rotation_value : Vector3
 func _ready() -> void:
 	set_inactive()
+	
 
 func _physics_process(delta: float) -> void:
+	asteroid.rotate(rotation_value, 0.005)
 	var collision := move_and_collide(transform.basis.z * speed * delta)
 	#if collision:
 		#collision.get_collider().damage(damage)
@@ -30,6 +36,8 @@ func death() -> void:
 
 func start() -> void:
 	set_active()
+	set_random_rotation()
+	speed = randf_range(BASE_SPEED - 1, BASE_SPEED + 1)
 	await get_tree().create_timer(lifetime).timeout
 	set_inactive()
 
@@ -40,3 +48,6 @@ func set_inactive() -> void:
 	set_physics_process(false)
 	global_position = Vector3(0,0,10)
 	get_parent().available_obstacle_pool.append(self)
+
+func set_random_rotation() -> void:
+	rotation_value = Vector3(randf(),randf(),randf()).normalized()
